21e8b4488feb025b74e67465eecbdb3eab10dca4
Mitsuaki Sato <sato@c2c-platform.com>
2023-02-10 20:59:59 +0900

    fix: eslint

--- src/components/layouts/sidebar/SidebarLayout.stories.tsx
+++ src/components/layouts/sidebar/SidebarLayout.stories.tsx
@@ -10,7 +10,7 @@ export default {
 } as ComponentMeta<typeof SidebarLayout>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof SidebarLayout> = (args) => (
+const Template: ComponentStory<typeof SidebarLayout> = (_args) => (
   <SidebarLayout />
 );
 
